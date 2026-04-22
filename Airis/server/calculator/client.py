a8821c65492f461d2a02d029c519990499ef17a1
Zostime <2272247653@qq.com>
2026-04-23 05:09:05 +0800

    Deleted Tool and switched to using the WebSocket Airis API to call OpenAI Function_calling

---
+++ Airis/server/calculator/client.py
@@ -0,0 +1,49 @@
+from airis_sdk import Websocket, Action
+from simpleeval import simple_eval
+import json
+import asyncio
+
+async def main():
+    client = Websocket()
+    await client.connect("ws://localhost:9000/game")
+    await client.startup("calculator")
+
+    await client.register_actions([
+        Action(
+            name="calculator",
+            description="计算数学表达式",
+            schema={
+                "type": "object",
+                "properties": {
+                    "expression": {
+                        "type": "string",
+                        "description": "数学表达式,例如:2+2"
+                    }
+                },
+                "required": ["expression"]
+            }
+        ).to_dict()
+    ])
+
+    async def on_action(payload):
+        action_name=payload.get("name")
+        action_id=payload.get("id")
+        data=json.loads(payload.get("data"))
+
+        if action_name=="calculator":
+            try:
+                res=simple_eval(data.get("expression"))
+                await client.send_action_result(action_id,True, res)
+            except Exception as e:
+                await client.send_action_result(action_id,False, f"error: {e}")
+
+    client.on_action(on_action)
+    loop = asyncio.Future()
+    await loop
+    await client.disconnect()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        exit()
---
+++ Airis/server/sound_board/client.py
@@ -0,0 +1,53 @@
+from airis_sdk import Websocket, Action
+from playsound3 import playsound
+import os
+from pathlib import Path
+import json
+import asyncio
+
+audios_dir = f"{Path(__file__).parent}/audios"
+
+async def main():
+    client = Websocket()
+    await client.connect("ws://localhost:9000/game")
+    await client.startup("calculator")
+
+    await client.register_actions([
+        Action(
+            name="sound_board",
+            description="播放指定音效",
+            schema={
+                "type": "object",
+                "properties": {
+                    "audio": {
+                        "type": "string",
+                        "description": f"音频名称,只可以是{os.listdir(audios_dir)}中的任意一个"
+                    }
+                },
+                "required": ["audio"]
+            }
+        ).to_dict()
+    ])
+
+    async def on_action(payload):
+        action_name=payload.get("name")
+        action_id=payload.get("id")
+        data=json.loads(payload.get("data"))
+
+        if action_name=="sound_board":
+            try:
+                playsound(f"{audios_dir}/{data.get('audio')}")
+                await client.send_action_result(action_id,True,"播放成功")
+            except Exception as e:
+                await client.send_action_result(action_id,False, f"播放失败: {e}")
+
+    client.on_action(on_action)
+    loop = asyncio.Future()
+    await loop
+    await client.disconnect()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        exit()
